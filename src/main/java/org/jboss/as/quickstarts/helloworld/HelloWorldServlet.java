7245606f718f5447a8305753d6fda0532a0dccda
ganrad <gradhakr@nilanet.com>
2016-09-23 18:40:47 -0700

    09-23-2016_01: Updated the servlet code.

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -55,7 +55,9 @@ public class HelloWorldServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
+        // writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
+        String name = req.getParameter("name");
+        writer.println("<h1>" + helloService.createHelloMessage(name) + "</h1>");
         writer.println(PAGE_FOOTER);
         writer.close();
     }

fe97c5e91da20e9bb5fd8364722cd4dc12d385f3
ganrad <gradhakr@nilanet.com>
2016-09-27 12:47:14 -0700

    09-27-2016_00: First commit.

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -56,15 +56,15 @@ public class HelloWorldServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
-        /* String name = req.getParameter("name");
+        // writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
+        String name = req.getParameter("name");
         if ( (name == null) || name.equals("") )
         	name = "World";
         writer.println("<h1>" + helloService.createHelloMessage(name) + "</h1>");
         writer.println("<h2>OS Name: " + System.getProperty("os.name") + "</h2>");
         writer.println("<h2>OS Version: " + System.getProperty("os.version") + "</h2>");
         writer.println("<h2>Node/Server name: " + InetAddress.getLocalHost().getHostName() + "</h2>");
-        writer.println("<h2>Node/Server IP: " + InetAddress.getLocalHost().getHostAddress() + "</h2>"); */
+        writer.println("<h2>Node/Server IP: " + InetAddress.getLocalHost().getHostAddress() + "</h2>");
         writer.println(PAGE_FOOTER);
         writer.close();
     }
